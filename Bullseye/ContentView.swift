915317f069241789da62efb3600576fd6b09bf84
Tony Robertson <tonyr729@gmail.com>
2020-02-25 22:18:10 -0700

    Add POC code

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,8 +9,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var hitAlertIsVisible: Bool = false
+    @State var knockAlertIsVisible: Bool = false
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            VStack {
+                Text("Welcome to my first App!")
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.green)
+                
+                Button(action: {
+                    print("Button pressed!")
+                    self.hitAlertIsVisible = true;
+                }) {
+                    Text(/*@START_MENU_TOKEN@*/"Hit me!"/*@END_MENU_TOKEN@*/)
+                }
+                .alert(isPresented: $hitAlertIsVisible) { () ->
+                    Alert in
+                    return Alert(
+                        title: Text("Hello there!"),
+                        message: Text("This is my first popup!"),
+                        dismissButton: .default(Text("Awesome!"))
+                    )
+                }
+                
+                Button(action: {
+                    print("Knock button pressed!")
+                    self.knockAlertIsVisible = true;
+                }) {
+                    Text(/*@START_MENU_TOKEN@*/"Knock Knock!"/*@END_MENU_TOKEN@*/)
+                }
+                .alert(isPresented: $knockAlertIsVisible) { () ->
+                    Alert in
+                    return Alert(
+                        title: Text("Whos there?"),
+                        message: Text("Insert knock knock joke"),
+                        dismissButton: .default(Text("Ugh"))
+                    )
+                }
+            }
+        }
     }
 }
 
